9bea26ed2f2bbae466efd23f412becd3a14b4f11
shanemckeon <sdm63@pitt.edu>
2025-03-18 10:48:08 -0400

    reorg

--- psc_debug/01_sbatch.bash
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-#SBATCH --nodes=1
-#SBATCH --ntasks=2
-module load matlab
-matlab -nodisplay -r 'try, matlab_01_entry(4), catch(e), e, end, quit'
--- psc_debug/matlab_01_entry.m
+++
@@ -1,8 +0,0 @@
-function matlab_01_entry(n_parts)
-  data = 1:10;
-  fprintf('# starting matlab_02_func\n')
-  % 01_sbatch.bash: 4 parts, matlab_02_func: 2 pools
-  x = matlab_02_func(data, n_parts);
-  fprintf('# finished matlab_02_func\n')
-  disp(sum(x));
-end
